f404e86557b29031c449c67651001db8aff633c6
Denys Ivashchenko <yawdenisk@gmail.com>
2025-11-06 22:50:07 +0100

    added user-agent to rest template

--- backend/src/main/java/org/example/backend/Configuration/AppConfig.java
+++ backend/src/main/java/org/example/backend/Configuration/AppConfig.java
@@ -1,5 +1,6 @@
 package org.example.backend.Configuration;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .defaultHeader("User-Agent", "github-org-snapshot")
+                .build();
     }
 }
